b587691030fc460a399bd0b18a55a9090099114e
Debdutta Bhattacharya <deb@jojodelivery.com>
2018-07-09 10:25:38 +0530

    Project base checkin

---
+++ src/main/java/com/pin91/hrm/JojoHrmApplication.java
@@ -0,0 +1,12 @@
+package com.pin91.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoHrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JojoHrmApplication.class, args);
+	}
+}
